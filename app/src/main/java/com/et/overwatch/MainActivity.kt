934ad19e0a647fa61e053291bd2e2c5c3e6c0728
silent1603 <tamgiangbui@gmail.com>
2023-11-26 21:47:19 +0700

    Init project

---
+++ app/src/main/java/com/et/overwatch/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.et.overwatch
+
+import android.os.Bundle
+import android.view.SurfaceView
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.google.android.filament.utils.Utils
+
+class MainActivity : ComponentActivity() {
+
+    companion object {
+        init {
+            Utils.init()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AndroidView(factory = { context ->
+                        val renderer = ModelRenderer()
+
+                        SurfaceView(context).apply {
+                            renderer.onSurfaceAvailable(this, lifecycle)
+                        }
+                    })
+                }
+            }
+        }
+    }
+}
